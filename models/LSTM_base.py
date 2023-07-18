eaf6506d91e78c5dae663d1ef7b7261deebbf94f
Polumm <songchujiashenzhen@163.com>
2023-07-18 11:34:45 +0800

    23/7/18

--- models/LSTM_base.py
+++ models/LSTM_base.py
@@ -8,19 +8,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# 在模型的最后一层去掉unsqueeze操作
+
 class LSTM_base(nn.Module):
+    # 定义构造函数
     def __init__(self, input_dim, hidden_dim, output_dim=1, num_layers=2):
         super(LSTM_base, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim  # 设置隐藏层维度
+        self.num_layers = num_layers  # 设置 LSTM 的层数
 
+        # 定义 LSTM 网络，输入维度为 input_dim，隐藏层维度为 hidden_dim，层数为 num_layers
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        # 定义全连接层，将隐藏层的输出转化为最终的输出，输出维度为 output_dim
         self.fc = nn.Linear(hidden_dim, output_dim)
 
+    # 定义前向传播过程
     def forward(self, x):
+        # 初始化隐藏状态和细胞状态，维度均为 (层数, batch大小, 隐藏层维度)
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        # LSTM层前向传播，得到输出以及最新的隐藏状态和细胞状态
         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        # 将 LSTM 层的输出通过全连接层得到最终的输出
         out = self.fc(out[:, -1, :])
-        return out  # 去掉unsqueeze操作
+        return out
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -8,19 +8,21 @@
 import pandas as pd
 import numpy as np
 import datetime
-import pandas as pd
 
-# 创建一个 DataFrame 来存储实验结果
+# 创建一个 DataFrame 来存储数据处理的结果
 experiment_results = pd.DataFrame(columns=['hidden_size', 'prediction_step', 'features', 'data_split', 'normalization', 'final_train_loss', 'final_test_loss', 'final_train_rmse', 'final_test_rmse'])
 
 def load_and_preprocess_data(filepath, prediction_step, features):
+    # 从指定路径加载数据，并进行异常记录剔除与空值填补
     data = pd.read_csv(filepath)
     data = data.iloc[24:].copy()
     data.fillna(method='ffill', inplace=True)
     data.drop('No', axis=1, inplace=True)
+    # 创建新的 'time' 列，代表每行数据的时间戳，并设置为索引
     data['time'] = data.apply(lambda x: datetime.datetime(year=x['year'], month=x['month'], day=x['day'], hour=x['hour']),axis=1)
     data.set_index('time', inplace=True)
     data.drop(columns=['year', 'month', 'day', 'hour'], inplace=True)
+    # 添加列名并将将 'cbwd' 列的值转换为独热编码，并加入到 DataFrame 中
     data.columns = ['pm2.5', 'dew', 'temp', 'press', 'cbwd', 'iws', 'snow', 'rain']
     data = data.join(pd.get_dummies(data.cbwd))
     del data['cbwd']
@@ -28,23 +30,28 @@ def load_and_preprocess_data(filepath, prediction_step, features):
     # 选择特定的特征列
     data = data[features]
 
+    # 设置序列长度，由预测步数决定
     sequence_length = prediction_step * 24  # 将原来固定的sequence_length修改为prediction_step*24
     delay = prediction_step
     data_ = []
+    # 生成具有特定序列长度的连续时间序列样本
     for i in range(len(data) - sequence_length - delay):
         df = data.iloc[i: i + sequence_length + delay].copy()
+        # 对每个序列样本进行标准化
         mean = df.mean()
         std = df.std()
         df = (df - mean) / std
         data_.append(df)
     data_ = np.array([df.values for df in data_])
 
+    # 切分输入和输出
     np.random.shuffle(data_)
     x = data_[:, :-delay, :]
     y = data_[:, -delay:, 0]
     x = x.astype(np.float32)
     y = y.astype(np.float32)
 
+    # 划分训练集和测试集
     split_boundary = int(data_.shape[0] * 0.8)
     train_x = x[: split_boundary]
     test_x = x[split_boundary:]
@@ -52,4 +59,5 @@ def load_and_preprocess_data(filepath, prediction_step, features):
     test_y = y[split_boundary:]
 
     return train_x, test_x, train_y, test_y
+
 
